510b0f5a6271776d3757c8ea8786adc876c37ef7
NikitaUdav <1nikitaudav@gmail.com>
2020-06-13 15:20:46 +0300

    alfa

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,15 @@ const config = {
   type: "carousel",
   startAt: 0,
   gap: 30,
-  animationDuration: 500,
+  animationDuration: 5000,
+};
+const config2 = {
+  type: "carousel",
+  startAt: 0,
+  gap: 30,
+  animationDuration: 5000,
+  autoplay: 5000,
+  hoverpause: true,
 };
 new Glide(".glide", config).mount();
-new Glide(".glide2", config).mount();
+new Glide(".glide2", config2).mount();
