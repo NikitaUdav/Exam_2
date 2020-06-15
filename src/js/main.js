663c165ca53a339ba3de2529f293994e0dc93ebd
NikitaUdav <1nikitaudav@gmail.com>
2020-06-15 12:47:16 +0300

    done

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,18 @@ const config2 = {
 };
 new Glide(".glide", config).mount();
 new Glide(".glide2", config2).mount();
+var mymap = L.map("mapid").setView([40.6512, -73.8504], 13);
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1IjoicnVkb2xmdG9sc2tpeSIsImEiOiJja2JnOHJ6OG4xMzRqMzBwOGhsa3B0bmw3In0.8itngzwV6L7DW9jnFYpqQw",
+  }
+).addTo(mymap);
+var marker = L.marker([40.6712, -73.8504]).addTo(mymap);
